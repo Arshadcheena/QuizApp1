1af30c449a4572519fea8b871bcbc207ffea6bba
Cheena <arshad424253816@gmail.com>
2023-12-26 12:11:09 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,89 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "What is the capital city of France?",
+            // Add more questions below
+            "What is the largest mammal on Earth?",
+            "Who is the author of 'To Kill a Mockingbird'?",
+            "What is the powerhouse of the cell?",
+            "In which year did World War II end?",
+            "What is the currency of Japan?",
+            "Who painted the Mona Lisa?",
+            "What is the chemical symbol for gold?",
+            "What is the speed of light?",
+            "What is the largest planet in our solar system?",
+            "What is the square root of 144?",
+            "Simplify: 3x + 5 = 14",
+            "What is the value of π (pi) to two decimal places?",
+            // ... (add more questions)
+            "What is the primary programming language for Android app development?",
+            "Which programming language is known for its simplicity and readability?",
+            "What does HTML stand for?",
+            "What is the purpose of CSS in web development?",
+            "What does JVM stand for in the context of Java programming?",
+            "What is the main function of a router in a computer network?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Berlin","Madrid","Paris","Rome"},
+            // Add more choices below
+            {"Elephant","Blue Whale","Giraffe","Lion"},
+            {"Harper Lee","J.K. Rowling","George Orwell","Ernest Hemingway"},
+            {"Nucleus","Mitochondria","Ribosome","Endoplasmic Reticulum"},
+            {"1945", "1950", "1939", "1941"},
+            {"Yen", "Won", "Yuan", "Ringgit"},
+            {"Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso", "Michelangelo"},
+            // ... (add more choices)
+            {"Au", "Ag", "Fe", "Hg"},
+            {"299,792 kilometers per second", "150,000 kilometers per second", "200,000 kilometers per second", "250,000 kilometers per second"},
+            {"Jupiter", "Saturn", "Mars", "Earth"},
+            {"10", "12", "14", "16"},
+            {"x = 3", "x = 4", "x = 5", "x = 6"},
+            {"3.14", "3.16", "3.18", "3.20"},
+            // ... (add more choices)
+            {"Java","Kotlin","C++","Swift"},
+            {"Java","Python","C#","JavaScript"},
+            {"Hypertext Markup Language","Hyper Transfer Markup Language","High-Level Text Markup Language","Home Tool Markup Language"},
+            {"To define the structure and layout of a web page","To handle server-side logic","To define the behavior of a web page","To store data on the client-side"},
+            {"Java Virtual Machine","Java Visual Machine","Java Virtual Memory","Java Verified Machine"},
+            {"To connect multiple computers in a network","To provide internet connectivity","To control the flow of data between devices","To store and manage data in a network"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "Paris",
+            // Add more correct answers below
+            "Blue Whale",
+            "Harper Lee",
+            "Mitochondria",
+            "1945",
+            "Yen",
+            "Leonardo da Vinci",
+            "Au",
+            "299,792 kilometers per second",
+            "Jupiter",
+            "12",
+            "x = 3",
+            "3.14",
+            // ... (add more correct answers)
+            "Java",
+            "Python",
+            "Hypertext Markup Language",
+            "To define the structure and layout of a web page",
+            "Java Virtual Machine",
+            "To connect multiple computers in a network"
+    };
+}
